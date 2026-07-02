adfd09d7a70d96134bcf6bef155c798602545b8e
Deployer <deploy@example.com>
2026-07-03 00:49:37 +0530

    Revert auto-enrollment, enforce course purchase

--- server.js
+++ server.js
@@ -96,14 +96,11 @@ app.post('/api/firebase-login', async (req, res) => {
     const name = decodedToken.name || email.split('@')[0];
 
     // 2. Find or create user in MongoDB
-    let user = { name, email, enrolledCourses: ['cs-gate-pyq'] };
+    let user = { name, email, enrolledCourses: [] };
     if (mongoose.connection.readyState === 1) {
       let dbUser = await User.findOne({ email });
       if (!dbUser) {
-        dbUser = await User.create({ name, email, enrolledCourses: ['cs-gate-pyq'] });
-      } else if (!dbUser.enrolledCourses.includes('cs-gate-pyq')) {
-        dbUser.enrolledCourses.push('cs-gate-pyq');
-        await dbUser.save();
+        dbUser = await User.create({ name, email, enrolledCourses: [] });
       }
       user = { name: dbUser.name, email: dbUser.email, enrolledCourses: dbUser.enrolledCourses, _id: dbUser._id };
     }
