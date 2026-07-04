c16b1eaf84b3b247d3076f8da4ec509b9af5d395
Deployer <deploy@example.com>
2026-07-05 00:52:59 +0530

    update paise

--- js/app.js
+++ js/app.js
@@ -193,8 +193,8 @@ window.testSeries = [
       // ["FST - 3", "Nov 22, 2025"],
       // ["FST - 2", "Nov 21, 2025"],
     ],
-    price: "₹10",
-    basePrice: 10,
+    price: "₹119",
+    basePrice: 119,
     brandLabel: "APEX EASY"
   },
   {
