35dd8d64ebf9dcbfc90f29945a0d827dff36444c
Deployer <deploy@example.com>
2026-07-04 21:58:59 +0530

    new questions

--- js/test-registry.js
+++ js/test-registry.js
@@ -77,84 +77,9 @@ registerTest({
         '<img src="images/digital logic-1/mceclip2-1777716934657.png" alt="Option B" style="max-width:150px;">',
         '<img src="images/digital logic-1/mceclip3-1777716941485.png" alt="Option C" style="max-width:150px;">',
         '<img src="images/digital logic-1/mceclip4-1777716946265.png" alt="Option D" style="max-width:150px;">',],
-      answer: "A",
+      answer: "B",
       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
     },
-    {
-      marks: 1,
-      neg: 0,
-      type: "MSQ",
-      text: "Consider the MUX circuit shown in the figure below: The output function ‘f’ can be represented as",
-      image: 'images/digital logic-1/mceclip12-1777717060063.png',
-      options: [
-        '<img src= "images/digital logic-1/mceclip13-1777717078783.png"alt="Option A" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip14-1777717083378.png" alt="Option B" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option C" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option D" style="max-width:150px;">',
-      ],
-      answer: ["A", "B"],
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
-    },
-    {
-      marks: 1,
-      neg: 0,
-      type: "NAT",
-      text: "Consider a 3-bit number A and 2 bit number B are given to a multiplier. The output of multiplier is realized using AND gate and one bit full adders. If minimum number of AND gates required are X and one bit full adders required are Y, then X + Y = _______.",
-      image: "images/digital logic-1/mceclip18-1777717146503.png",
-      options: [],
-      answer: 9,
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
-    }
-  ]
-});
-
-registerTest({
-  series: "cs-gate-pyq",
-  name: "TWT - Database-2",
-  date: "May 20, 2026",
-  questions: [
-    {
-      marks: 1,      // Add correct marks here
-      neg: 0.33,     // Add negative marks here
-      type: "MCQ",
-      text: "Is this the easiest way to add a test?",
-      options: ["Yes, very easy", "No, it's hard", "Maybe", "I don't know"],
-      answer: "A",
-      solution: "This is Option 2 in action! Just 1 step."
-    },
-    {
-      marks: 2,
-      neg: 0,
-      type: "MSQ",
-      text: "Consider the MUX circuit shown in the figure below: The output function ‘f’ can be represented as",
-      image: 'images/digital logic-1/mceclip12-1777717060063.png',
-      options: [
-        '<img src= "images/digital logic-1/mceclip13-1777717078783.png"alt="Option A" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip14-1777717083378.png" alt="Option B" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option C" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option D" style="max-width:150px;">',
-      ],
-      answer: ["A", "B"],
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
-    },
-    {
-      marks: 2,
-      neg: 0,
-      type: "NAT",
-      text: "Consider a 3-bit number A and 2 bit number B are given to a multiplier. The output of multiplier is realized using AND gate and one bit full adders. If minimum number of AND gates required are X and one bit full adders required are Y, then X + Y = _______.",
-      image: "images/digital logic-1/mceclip18-1777717146503.png",
-      options: [],
-      answer: 9,
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
-    }
-  ]
-});
-
-registerTest({
-  series: "cs-gate-pyq",
-  name: "TWT - computer network-1",
-  date: "May 22, 2026",
-  questions: [
     {
       marks: 1,
       neg: 0.33,
@@ -167,23 +92,23 @@ registerTest({
         '<img src="images/digital logic-1/mceclip10-1777717028350.png" alt="Option C" style="max-width:150px;">',
         '<img src="images/digital logic-1/mceclip9-1777717022578.png" alt="Option D" style="max-width:150px;">',
       ],
-      answer: "B",
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+      answer: "C",
+      solution: "<img src='images/digital logic-1/mceclip11-1777717035859.png' alt='Detailed Solution'>",
     },
     {
       marks: 1,
-      neg: 0,
-      type: "MSQ",
+      neg: 0.33,
+      type: "MCQ",
       text: "Consider the MUX circuit shown in the figure below: The output function ‘f’ can be represented as",
       image: 'images/digital logic-1/mceclip12-1777717060063.png',
       options: [
         '<img src= "images/digital logic-1/mceclip13-1777717078783.png"alt="Option A" style="max-width:150px;">',
         '<img src="images/digital logic-1/mceclip14-1777717083378.png" alt="Option B" style="max-width:150px;">',
         '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option C" style="max-width:150px;">',
-        '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option D" style="max-width:150px;">',
+        '<img src="images/digital logic-1/mceclip16-1777717095119.png" alt="Option D" style="max-width:150px;">',
       ],
-      answer: ["A", "B"],
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+      answer: "B",
+      solution: "<img src='images/digital logic-1/mceclip17-1777717104840.png' alt='Detailed Solution'>",
     },
     {
       marks: 1,
@@ -193,8 +118,138 @@ registerTest({
       image: "images/digital logic-1/mceclip18-1777717146503.png",
       options: [],
       answer: 9,
-      solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+      solution: "<img src='images/digital logic-1/mceclip19-1777717155306.png' alt='Detailed Solution'>",
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: 'The Boolean expression for bulb ‘B’ shown in figure below is',
+      image: "images/digital logic-1/mceclip20-1777717191268.png",
+      options: [
+        '<img src= "images/digital logic-1/mceclip22-1777717204356.png"alt="Option A" style="max-width:150px;">',
+        '<img src="images/digital logic-1/mceclip21-1777717217418.png" alt="Option B" style="max-width:150px;">',
+        '<img src="images/digital logic-1/mceclip21-1777717198121.png" alt="Option C" style="max-width:150px;">',
+        '<img src="images/digital logic-1/mceclip23-1777717209338.png" alt="Option D" style="max-width:150px;">',
+      ],
+      answer: "A",
+      solution: "<img src='images/digital logic-1/mceclip25-1777717226881.png' alt='Detailed Solution'>",
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: "Consider the circuit shown in the figure below: If the three bit input to the circuit is (X2 X1 X0) = 101, then the decimal equivalent of the corresponding output of the circuit (Y2 Y1 Y0) will be equal to _________ (decimal form).",
+      image: "images/digital logic-1/mceclip26-1777717255870.png",
+      options: [],
+      answer: 3,
+      solution: "<img src='images/digital logic-1/mceclip27-1777717310534.png' alt='Detailed Solution'>",
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MSQ",
+      text: "Two half adders are connected in cascade as shown below, the output ‘S’ and ‘C’ is/are",
+      image: "images/digital logic-1/mceclip29-1777717373795.png",
+      options: [
+        "images/digital logic-1/mceclip32-1777717389924.png",
+        "images/digital logic-1/mceclip30-1777717379835.png",
+        "images/digital logic-1/mceclip33-1777717395424.png",
+        "images/digital logic-1/mceclip31-1777717384695.png",
+      ],
+      answer: ["A", "D"],
+      solution: "<img src='images/digital logic-1/mceclip34-1777717400773.png' alt='Detailed Solution'>",
     }
   ]
 });
+
+// registerTest({
+//   series: "cs-gate-pyq",
+//   name: "TWT - Database-2",
+//   date: "May 20, 2026",
+//   questions: [
+//     {
+//       marks: 1,      // Add correct marks here
+//       neg: 0.33,     // Add negative marks here
+//       type: "MCQ",
+//       text: "Is this the easiest way to add a test?",
+//       options: ["Yes, very easy", "No, it's hard", "Maybe", "I don't know"],
+//       answer: "A",
+//       solution: "This is Option 2 in action! Just 1 step."
+//     },
+//     {
+//       marks: 2,
+//       neg: 0,
+//       type: "MSQ",
+//       text: "Consider the MUX circuit shown in the figure below: The output function ‘f’ can be represented as",
+//       image: 'images/digital logic-1/mceclip12-1777717060063.png',
+//       options: [
+//         '<img src= "images/digital logic-1/mceclip13-1777717078783.png"alt="Option A" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip14-1777717083378.png" alt="Option B" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option C" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option D" style="max-width:150px;">',
+//       ],
+//       answer: ["A", "B"],
+//       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+//     },
+//     {
+//       marks: 2,
+//       neg: 0,
+//       type: "NAT",
+//       text: "Consider a 3-bit number A and 2 bit number B are given to a multiplier. The output of multiplier is realized using AND gate and one bit full adders. If minimum number of AND gates required are X and one bit full adders required are Y, then X + Y = _______.",
+//       image: "images/digital logic-1/mceclip18-1777717146503.png",
+//       options: [],
+//       answer: 9,
+//       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+//     }
+//   ]
+// });
+
+// registerTest({
+//   series: "cs-gate-pyq",
+//   name: "TWT - computer network-1",
+//   date: "May 22, 2026",
+//   questions: [
+//     {
+//       marks: 1,
+//       neg: 0.33,
+//       type: "MCQ",
+//       text: "he Boolean expression for the output ‘F’ of the logic circuit shown in the figure below is",
+//       image: 'images/digital logic-1/mceclip6-1777717003260.png',
+//       options: [
+//         '<img src="images/digital logic-1/mceclip7-1777717008801.png" alt="Option A" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip8-1777717014766.png" alt="Option B" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip10-1777717028350.png" alt="Option C" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip9-1777717022578.png" alt="Option D" style="max-width:150px;">',
+//       ],
+//       answer: "B",
+//       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+//     },
+//     {
+//       marks: 1,
+//       neg: 0,
+//       type: "MSQ",
+//       text: "Consider the MUX circuit shown in the figure below: The output function ‘f’ can be represented as",
+//       image: 'images/digital logic-1/mceclip12-1777717060063.png',
+//       options: [
+//         '<img src= "images/digital logic-1/mceclip13-1777717078783.png"alt="Option A" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip14-1777717083378.png" alt="Option B" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option C" style="max-width:150px;">',
+//         '<img src="images/digital logic-1/mceclip15-1777717088021.png" alt="Option D" style="max-width:150px;">',
+//       ],
+//       answer: ["A", "B"],
+//       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+//     },
+//     {
+//       marks: 1,
+//       neg: 0,
+//       type: "NAT",
+//       text: "Consider a 3-bit number A and 2 bit number B are given to a multiplier. The output of multiplier is realized using AND gate and one bit full adders. If minimum number of AND gates required are X and one bit full adders required are Y, then X + Y = _______.",
+//       image: "images/digital logic-1/mceclip18-1777717146503.png",
+//       options: [],
+//       answer: 9,
+//       solution: "<img src='images/digital logic-1/mceclip5-1777716956632.png' alt='Detailed Solution'>",
+//     }
+//   ]
+// });
 
