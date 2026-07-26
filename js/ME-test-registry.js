33d6cbb01bfea7a588a1f4445b823b51d32b4ceb
Raj Verma <rajver8443@gmail.com>
2026-07-26 20:28:55 +0530

    Update ME-test-registry.js

--- js/ME-test-registry.js
+++ js/ME-test-registry.js
@@ -1,3 +1,243 @@
 // =========================================================================
 // ME TEST REGISTRY
 // =========================================================================
+registerTest({
+  series: "me-gate-pyq",
+  name: "TWT - Engineering mathematics-1",
+  date: "July 17, 2026",
+  questions: [
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `Consider the differential equation, <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_12.png"/>is equal to ______.`,
+      image: "",
+      options: [],
+      answer: "3",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_13.png"/><br/><br/>
+                 <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_14.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MSQ",
+      text: `Let <em>M</em> be a 3 × 3 invertible matrix with real entries and let <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_21.png"/> denote the 3 × 3 identity matrix. If M<sup>–1</sup> = adj (adj <em>M</em>), then which of the following statement is/are always TRUE?`,
+      image: "",
+      options: [
+      `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_22.png"/>`,
+      `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_23.png"/>`,
+      `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_24.png"/>`,
+      `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_25.png"/>`
+      ],
+      answer: ["B", "C", "D"],
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_26.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `If A is a square matrix of order three with its characteristic equation: <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_27.png"/>then its determinant is`,
+      image: "",
+      options: [],
+      answer: "-2",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_28.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `The work done by the conservative vector field <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_29.png"/> along any smooth curve <em>C</em> joining the point A(–1, 3, 9) to B(1, 6, –4) is`,
+      image: "",
+      options: [
+        `3`,
+        `4`,
+        `6`,
+        `12`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_30.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `The number of solutions of the system of equations: <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_31.png"/> and <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_32.png"/>is`,
+      image: "",
+      options: [
+        `2`,
+        `1`,
+        `0`,
+        `Infinite`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_33.png"/><br/><br/>
+      <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_34.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `The number of values of ‘c’ with reference to Lagrange’s mean value theorem for the function <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_35.png"/>is`,
+      image: "",
+      options: [
+        `Only one`,
+        `At least one but finite`,
+        `Infinite`,
+        `Zero`
+      ],
+      answer: "C",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_36.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `The function, <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_49.png"/> has a local maximum value at <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_50.png"/>equals to`,
+      image: "",
+      options: [
+        `-2`,
+        `-1`,
+        `3`,
+        `4`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_51.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `The determinant of the given matrix is ____________. <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_55.png"/><br/><br/>`,
+      image: "",
+      options: [],
+      answer: "12",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_56.png"/><br/><br/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `The Newton-Raphson iterative formula for finding <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_57.png"/>is`,
+      image: "",
+      options: [
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_58.png"/> <br/><br/><img absmiddle="" align="align="/><br/><br/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_59.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_60.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_61.png"/>`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_62.png"/><br/><br/>
+      <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_63.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "MSQ",
+      text: `Which of the following functions are continuous on (0, π)?`,
+      image: "",
+      options: [
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_1.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_2.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_3.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_4.png"/>`
+      ],
+      answer: ["B", "C"],
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_5.png"/><br/><br/>
+      <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_6.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_7.png"/>`,
+      image: "",
+      options: [
+        `2`,
+        `-2`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_8.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_9.png"/>`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_10.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `The area bounded by the curve x<sup>2</sup> = 4y and the straight line x = 4y – 2 is _____ unit<sup>2</sup>. (Correct upto three decimal places)`,
+      image: "",
+      options: [],
+      answer: "1.125",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_11.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `If matrix <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_15.png"/> is orthogonal, then the value of <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_16.png"/>is`,
+      image: "",
+      options: [],
+      answer: "1",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_17.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `Let <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_18.png"/> be the identity matrix of order 3. If <em>Q</em> = [<em>q<sub>ij</sub></em>] is a matrix, such that<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_19.png"/>equals`,
+      image: "",
+      options: [
+        `52`,
+        `103`,
+        `201`,
+        `205`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_20.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "MSQ",
+      text: `The eigen vectors of matrix <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_37.png"/>is/are`,
+      image: "",
+      options: [
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_38.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_39.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_40.png"/>`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_41.png"/>`
+      ],
+      answer: ["A", "B", "C"],
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_42.png"/><br/><br/>
+      <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_43.png"/><br/><br/>
+      <br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_44.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_45.png"/> be continuous in [0, ∞), then the most suitable values of a and b are:`,
+      image: "",
+      options: [
+        `<em>a</em> = –1 and <em>b</em> = 1`,
+        `<img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_46.png"/>`,
+        `<em>a</em> = 1 and <em>b</em> = –1`,
+        `None of these`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_47.png"/><br/><br/>
+<br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_48.png"/><br/><br/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `The directional derivative of the function <img align="absmiddle" alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_52.png"/> at the point <em>A</em> (1, 2, 3) in the direction of the line <em>AB</em> where <em>B</em> is the point (5, 0, 4) is ______. (Correct to two decimal places)`,
+      image: "",
+      options: [],
+      answer: "2.61",
+      solution: `<img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_53.png"/><br/><br/>
+<br/><br/><img alt="" src="js/questions/topicwise_test9_part_syllabus_gate_2026_me_engineering_mathematics1/img_54.png"/><br/><br/>`
+    }
+  ]
+});
