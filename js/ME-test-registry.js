c6d0820ca232a1d55ac90730f2c16c2e22e96309
Raj Verma <rajver8443@gmail.com>
2026-07-26 20:53:06 +0530

    Update ME-test-registry.js

--- js/ME-test-registry.js
+++ js/ME-test-registry.js
@@ -241,3 +241,236 @@ registerTest({
     }
   ]
 });
+
+registerTest({
+  series: "me-gate-pyq",
+  name: "TWT - Machine Design-2",
+  date: "July 15, 2026",
+  questions: [
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `Two plates of 10 mm thickness are jointed by single riveted lap joint with pitch 30 mm and hole diameter 15 mm. The allowable tearing stress of plate is 90 MPa, shear stress of rivet is 60 MPa and allowable crushing stress between rivet and the plate is 110 MPa. The joint efficiency is`,
+      image: "",
+      options: [
+        `52.67%`,
+        `46.23%`,
+        `42.32%`,
+        `39.27%`
+      ],
+      answer: "D",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_1.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `Match <strong>List-I</strong> (Bearings) with <strong>List-II</strong> (Purpose) and select the correct answer using the codes given below the lists: <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_2.png"/>`,
+      image: "",
+      options: [
+        `a`,
+        `b`,
+        `c`,
+        `d`
+      ],
+      answer: "B",
+      solution: `b`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `In a bolt with the shank turned down to the core diameter of the thread, which of the following statements is most accurate regarding its mechanical behaviour?`,
+      image: "",
+      options: [
+        `The stress concentration near the threads increases.`,
+        `The bolt becomes weaker but easy to manufacture.`,
+        `The bolt exhibits increased modulus of resilience and a uniform strength.`,
+        `The reduced shank diameter has no effect on stress distribution in the bolt.`
+      ],
+      answer: "C",
+      solution: `If the shaft of the bolt is turned down to a diameter equal to or even slightly less than the core diameter of the thread (<em>D<sub>c</sub></em>), then shank of the bolt will undergo a higher stress. This means that a shaft will absorb a large portion of the energy, thus relieving the material at the sections near the thread. The bolt in this way, becomes stronger and lighter and it increase the shock absorbing capacity of the bolt because of an increased modulus of resilience. This gives us bolts of uniform strength.`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `A plot between coefficient of friction and bearing characteristic number for a Journal bearing is given below <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_3.png"/> The oil film will break and there will be metal to metal contact when`,
+      image: "",
+      options: [
+        `Bearing characteristic number is less than K.`,
+        `Bearing characteristic number is greater than K.`,
+        `Bearing characteristic number is equal to K.`,
+        `Bearing characteristic number is equal to bearing modulus.`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_4.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `A closed coil helical compression spring of 15 active coils has a spring stiffness of K. It is cut into two springs having 11 and 4 turns. What will be the ratio of spring stiffnesses of the two resulting springs?`,
+      image: "",
+      options: [
+        `1.87`,
+        `1.57`,
+        `2.75`,
+        `2.25`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_5.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `A 45 mm diameter solid shaft is welded to a flat plate by 12 mm fillet weld as shown in the figure. If the maximum shear intensity in the weld material is not to exceed 75 MPa, the maximum torque that the weld can sustain is _____ kN-m. (Correct upto three decimal places) <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_6.png"/>`,
+      image: "",
+      options: [],
+      answer: "2.023",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_7.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `A shaft rotating at constant speed is subjected to variable load. The ball bearings supporting shaft are subjected to stationary equivalent radial load of 3 kN for 20% of time 2 kN for 30% time 1 kN for remaining time of cycle. The equivalent radial load will be _____ kN. (Correct upto 3 decimal places)`,
+      image: "",
+      options: [],
+      answer: "2.025",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_8.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `In a steam engine the maximum steam pressure is 1.5 N/mm<sup>2</sup> absolute and the back pressure is 0.5 N/mm<sup>2</sup> absolute. The cylinder diameter is 0.3 m. If allowable stress in mild steel is 45 N/mm<sup>2</sup> in tension. The diameter of the screwed end of the piston rod will be _____ mm. (Correct upto two decimal places)`,
+      image: "",
+      options: [],
+      answer: "44.72",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_9.png"/>`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MSQ",
+      text: `Which of the following statements about the Lewis form factor is correct?`,
+      image: "",
+      options: [
+        `It is a dimensionless parameter that accounts for tooth geometry.`,
+        `It depends on the number of teeth and pressure angle.`,
+        `It increases with increasing module.`,
+        `It increases with decreasing module.`
+      ],
+      answer: ["A", "B"],
+      solution: `Some important points about Lewis form factor are:<br/>• It is used in Lewis equation to compute bending stress in gear tooth.<br/>• It is dimensionless and purely geometric.<br/>• Depends on number of teeth, pressure angle, addendum.<br/>• It is independent of module.`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `Two plates of 8 mm thickness each are to be joined by means of a single riveted double strap butt joint. The diameter of the rivets is 18 mm and that of the rivet hole is 19 mm. If the working stress in shearing and tension are 60 MPa and 85 MPa respectively. Then the pitch of the rivet is`,
+      image: "",
+      options: [
+        `58 mm`,
+        `62 mm`,
+        `64 mm`,
+        `76 mm`
+      ],
+      answer: "C",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_10.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `A safety valve of 50 mm diameter is to blow off at a pressure of 1 MPa. It is held on its seat by a close coiled helical spring providing an initial compression of 30 mm. The maximum lift of the valve is 10 mm. If the maximum shear stress in the material of the wire is limited to 500 MPa, the modulus of rigidity for the spring material is 80 kN/mm<sup>2</sup> and spring index is 6. Then, the diameter of the wire is [Use Wahl’s factor, K = <img align="absmiddle" alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_11.png"/> where <em>C</em> is the spring index]`,
+      image: "",
+      options: [
+        `10 mm`,
+        `12 mm`,
+        `14 mm`,
+        `16 mm`
+      ],
+      answer: "A",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_12.png"/> <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_13.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `A ball bearing has to carry a radial load of 9000 N and axial load of 4500 N. The shaft rotates at 1200 rpm and average life of 4500 hours is desired. Inner race rotates and service factor is 1.5. If radial load factor and axial load factor is 0.5 and 1.2 respectively. What will be the expected dynamic load capacity of the bearing? [Assume race rotation factor, <img align="absmiddle" alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_14.png"/>= 1]`,
+      image: "",
+      options: [
+        `48.76 kN`,
+        `59.65 kN`,
+        `67.59 kN`,
+        `72.83 kN`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_15.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `Two 120 mm wide and 12.5 mm thick steel plates are joined together by means of double transverse fillet welds as shown in the figure. If the maximum tensile stress for the plates and the welding materials should not exceed 120 N/mm<sup>2</sup> and the strength of the weld is equal to the strength of the plates, the required length the weld will be <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_16.png"/>`,
+      image: "",
+      options: [
+        `104.67 mm`,
+        `84.87 mm`,
+        `74.04 mm`,
+        `64.44 mm`
+      ],
+      answer: "B",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_17.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `A riveted joint is shown below have four equal rivets of 20 mm diameter each. The working shear stress for the rivets is 100 MPa. The value of P for the joint is ______ kN. (Correct upto two decimal places) <img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_18.png"/>`,
+      image: "",
+      options: [],
+      answer: "78.54",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_19.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `A shaft made of mild steel is required to transmit 90 kW at 280 rpm. The supported length of the shaft is 3 meters. It carries two pulleys each weighing 1600 N supported at a distance of 1 metre from the ends respectively. Assuming allowable shear stress to be 60 MPa. The diameter of the shaft is ______ mm. (Rounded off to next integer)`,
+      image: "",
+      options: [],
+      answer: "67",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_20.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `A Bronze spur pinion rotating at 650 rpm drives a cast iron spur gear at a transmission ratio of 4:1. The allowable static stresses for the bronze pinion and cast iron gear are 85 MPa and 105 MPa respectively. The pinion has 18 standard 20° full depth involute teeth of module 8 mm. The face width of both the gears is 85 mm. The power that can be transmitted from the stand point of strength is ______ kW. (Correct upto two decimal places) [Assume Lewis form factor for gear and pinion to be 0.44 and 0.31 respectively]`,
+      image: "",
+      options: [],
+      answer: "33.36",
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_21.png"/>`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "MSQ",
+      text: `A full journal bearing operating under a steady load has journal diameter of 50 mm and bearing length of 65 mm. Radial load on bearing is 2.5 kN and journal speed is 900 rpm. The radial clearance is 0.04 mm and viscosity of oil is 80 × 10<sup>–9</sup> Ns/mm<sup>2</sup>.`,
+      image: "",
+      options: [
+        `Sommerfeld number for the bearing is nearly 0.61.`,
+        `Power loss due to friction is 113.37 W.`,
+        `Power loss due to friction is 167.82 W.`,
+        `Sommerfeld number for the bearing is nearly 0.82.`
+      ],
+      answer: ["A", "B"],
+      solution: `<img alt="" src="js/questions/topicwise_test24_part_syllabus_gate_2026_me_machine_design2/img_22.png"/>`
+    }
+  ]
+});
