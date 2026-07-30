db071ac22374d2411c15eec7116b4592029d7977
Deployer <deploy@example.com>
2026-07-30 14:08:37 +0530

    chore: force add quiz-test-registry.src.js

---
+++ js/quiz-test-registry.src.js
@@ -0,0 +1,216 @@
+window.registerTest({
+  series: "quiz",
+  name: "WQT - Digital logic-1|Boolean algebra",
+  questions: [
+    {
+        "num": 1,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>In the circuit shown below, which logic function does this circuit generate?<br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q1_img1.png\" style=\"max-width: 100%;\"><br>&nbsp;</p>\n            \n            <p></p>",
+        "options": [
+            "OR",
+            "AND",
+            "NOR",
+            "NAND"
+        ],
+        "answer": "A"
+    },
+    {
+        "num": 2,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>The dual of the Boolean function \n                <script type=\"math/tex\" id=\"MathJax-Element-1\">\\boldsymbol{x}+\\boldsymbol{y} \\boldsymbol{z}</script> is:\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-2\">\\bar{x}(\\bar{y}+\\bar{z})</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-3\">x(y+z)</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-4\">x+y z</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-5\">\\bar{x}+\\bar{y} \\bar{z}</script>"
+        ],
+        "answer": "B"
+    },
+    {
+        "num": 3,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>Applying DeMorgan's theorem to the expression \n                <script type=\"math/tex\" id=\"MathJax-Element-6\">\\overline{\\overline{(\\boldsymbol{X}+\\boldsymbol{Y})}+\\overline{\\boldsymbol{Z}}}</script>\n                , we get\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-7\">(X+Y) Z</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-8\">(\\bar{X}+\\bar{Y}) \\bar{Z}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-9\">(\\bar{X}+\\bar{Y}) Z</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-10\">(X+Y) \\bar{Z}</script>"
+        ],
+        "answer": "A"
+    },
+    {
+        "num": 4,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>Refer to the following figure, If \n                <script type=\"math/tex\" id=\"MathJax-Element-11\">\\mathbf{S}_{1}=\\mathbf{1}</script> and \n                <script type=\"math/tex\" id=\"MathJax-Element-12\">\\mathbf{S}_{2}=\\mathbf{0}</script> what will be the\n                logic state at the output \n                <script type=\"math/tex\" id=\"MathJax-Element-13\">\\mathbf{X}</script> ?\n            </p>\n            <p><br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q4_img2.png\" style=\"max-width: 100%;\"></p>\n            <p>&nbsp;</p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-14\">\\mathbf{X}=\\mathbf{A}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-15\">\\mathbf{X}=\\mathbf{B}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-16\">\\mathbf{X}=\\mathbf{C}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-17\">\\mathbf{X}=\\mathbf{D}</script>"
+        ],
+        "answer": "C"
+    },
+    {
+        "num": 5,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>Refer to the following figure. If \n                <script type=\"math/tex\" id=\"MathJax-Element-18\">\\mathbf{A}=\\mathbf{0}</script> and \n                <script type=\"math/tex\" id=\"MathJax-Element-19\">\\mathbf{B}=\\mathbf{1}</script>, what will be the logic\n                states at \n                <script type=\"math/tex\" id=\"MathJax-Element-20\">\\mathbf{X}, \\mathbf{Y}</script> and \n                <script type=\"math/tex\" id=\"MathJax-Element-21\">\\mathbf{Z}</script> ?<br><br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q5_img3.png\" style=\"max-width: 100%;\"><br>&nbsp;\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-22\">\\mathrm{X}=1, \\mathrm{Y}=1, \\mathrm{Z}=0</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-23\">\\mathrm{X}=1, Y=0, Z=0</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-24\">\\mathrm{X}=0, \\mathrm{Y}=0, \\mathrm{Z}=1</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-25\">X=0, Y=1, Z=0</script>"
+        ],
+        "answer": "C"
+    },
+    {
+        "num": 6,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>The simplification of the Boolean expression \n                <script type=\"math/tex\" id=\"MathJax-Element-26\">(\\overline{\\overline{\\boldsymbol{A}} \\boldsymbol{B} \\overline{\\boldsymbol{C}}})+(\\overline{\\boldsymbol{A} \\overline{\\boldsymbol{B}} \\boldsymbol{C}})</script>\n                is\n            </p>\n            <p>&nbsp;</p>\n            \n            <p></p>",
+        "options": [
+            "0",
+            "1",
+            "<script type=\"math/tex\" id=\"MathJax-Element-27\">\\mathbf{A}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-28\">\\mathbf{B C}</script>"
+        ],
+        "answer": "B"
+    },
+    {
+        "num": 7,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>In the given truth table, \n                <script type=\"math/tex\" id=\"MathJax-Element-29\">f(x,y)</script> represent the Boolean function.<br>\n                <script type=\"math/tex; mode=display\" id=\"MathJax-Element-30\">\\begin{array}{|c|c|c|} \\hline x & y & f(x,y) \\\\ \\hline 0 & 0 & 1 \\\\ \\hline 0 & 1 & 0 \\\\ \\hline 1 & 0 & 0 \\\\ \\hline 1 & 1 & 1 \\\\ \\hline \\end{array}</script>\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-31\">x \\leftrightarrow y</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-32\"> x \\wedge y</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-33\">x \\vee y</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-34\">x \\rightarrow y</script>"
+        ],
+        "answer": "A"
+    },
+    {
+        "num": 8,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>\n                <script type=\"math/tex\" id=\"MathJax-Element-35\">(A+C’)(B’+C’)</script> simplifies to\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-36\">AC’+B’</script><br>&nbsp;",
+            "<script type=\"math/tex\" id=\"MathJax-Element-37\">C(A’+B’)</script><br>&nbsp;",
+            "<script type=\"math/tex\" id=\"MathJax-Element-38\">BC’+A</script><br>&nbsp;",
+            "<script type=\"math/tex\" id=\"MathJax-Element-39\">AB’+C’</script>"
+        ],
+        "answer": "D"
+    },
+    {
+        "num": 9,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>In digital logic, if \n                <script type=\"math/tex\" id=\"MathJax-Element-40\">A\\oplus B=C</script>, then which one of the following is\n                true?\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-41\">A\\oplus C=B</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-42\">B\\oplus C=A</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-43\">A\\oplus B\\oplus C=0</script>",
+            "Both (A) and (B)"
+        ],
+        "answer": "N"
+    },
+    {
+        "num": 10,
+        "type": "MCQ",
+        "marks": 1,
+        "penalty": 0.33,
+        "text": "<p></p>\n            <p>In Boolean algebra \n                <script type=\"math/tex\" id=\"MathJax-Element-44\">1+1+1+1\\dots\\dots 800</script> times ones \n                <script type=\"math/tex\" id=\"MathJax-Element-45\">=&nbsp; </script> ________&nbsp; .\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-46\">1</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-47\">0</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-48\">11</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-49\">800</script>"
+        ],
+        "answer": "A"
+    },
+    {
+        "num": 11,
+        "type": "MCQ",
+        "marks": 2,
+        "penalty": 0.67,
+        "text": "<p></p>\n            <p>Applying DeMorgan's Law to \n                <script type=\"math/tex\" id=\"MathJax-Element-50\">f=\\overline{\\overline{\\overline{A B}+\\bar{C}}(E+\\bar{D}})</script> will\n                result in:\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-51\">f=\\bar{A}+\\bar{B}+\\bar{C}+\\bar{E}+D</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-52\">f=\\bar{A} \\bar{B} \\bar{C}+E \\bar{D}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-53\">f=\\bar{A} \\bar{B} \\bar{C}+(E+\\bar{D})</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-54\">f=\\bar{A}+\\bar{B}+\\bar{C}+\\bar{E} D</script>"
+        ],
+        "answer": "D"
+    },
+    {
+        "num": 12,
+        "type": "MCQ",
+        "marks": 2,
+        "penalty": 0.67,
+        "text": "<p></p>\n            <p>The Boolean function obtained by adding an inverter to each and every input of an \n                <script type=\"math/tex\" id=\"MathJax-Element-55\">\\text{AND}</script> gate is:\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-56\">\\text{OR}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-57\">\\text{XOR}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-58\">\\text{NAND}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-59\">\\text{NOR}</script>"
+        ],
+        "answer": "D"
+    },
+    {
+        "num": 13,
+        "type": "MCQ",
+        "marks": 2,
+        "penalty": 0.67,
+        "text": "<p></p>\n            <p>What does the following circuit evaluate to?<br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q13_img4.png\" style=\"max-width: 100%;\"></p>\n            \n            <p></p>",
+        "options": [
+            "0",
+            "1",
+            "<script type=\"math/tex\" id=\"MathJax-Element-60\">A^{\\prime}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-61\">A</script>"
+        ],
+        "answer": "C"
+    },
+    {
+        "num": 14,
+        "type": "MCQ",
+        "marks": 2,
+        "penalty": 0.67,
+        "text": "<p></p>\n            <p>The logic circuit shown can be minimized to<br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q14_img5.png\" style=\"max-width: 100%;\"></p>\n            <p>&nbsp;</p>\n            <p>&nbsp;</p>\n            <p><br><img alt=\"\" src=\"/images/quiz/wqt-dl1/q14_img6.png\" style=\"max-width: 100%;\"></p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-62\">\\mathbf{A}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-63\">\\mathbf{B}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-64\">\\mathbf{C}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-65\">\\mathbf{D}</script>"
+        ],
+        "answer": "D"
+    },
+    {
+        "num": 15,
+        "type": "MCQ",
+        "marks": 2,
+        "penalty": 0.67,
+        "text": "<p></p>\n            <p>Simplification of the Boolean expression \n                <script type=\"math/tex\" id=\"MathJax-Element-66\">\\boldsymbol{A B}+\\boldsymbol{A B C}+\\boldsymbol{A B C D}+\\boldsymbol{A B C D E}+\\boldsymbol{A B C D E F}</script>\n                yields which of the following results?\n            </p>\n            \n            <p></p>",
+        "options": [
+            "<script type=\"math/tex\" id=\"MathJax-Element-67\">\\mathrm{AB}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-68\">\\mathrm{AB}+\\mathbf{C D}+\\mathbf{E F}</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-69\">A B C D E F</script>",
+            "<script type=\"math/tex\" id=\"MathJax-Element-70\">\\mathbf{A}+\\mathbf{B}+\\mathbf{C}+\\mathbf{D}+\\mathbf{E}+\\mathbf{F}</script>"
+        ],
+        "answer": "A"
+    }
+]
+});
