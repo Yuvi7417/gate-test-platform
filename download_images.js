b2356fdb4d78e15dbef8203f1779e6e38c9c2f35
Deployer <deploy@example.com>
2026-07-31 01:43:45 +0530

    fix image download redirects

--- download_images.js
+++ download_images.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
-const http = require('https');
+const http = require('http');
+const https = require('https');
 
 const html = fs.readFileSync('test.html', 'utf8');
 const $ = cheerio.load(html);
@@ -11,13 +12,18 @@ $('.res_question').each((i, qDiv) => {
   const num = $(qDiv).find('.res_qs_num b').text().replace('Q #', '').trim();
   
   $(qDiv).find('.res_question_text img').each((j, img) => {
-    const src = $(img).attr('src');
+    let src = $(img).attr('src');
     if (src && src.startsWith('http')) {
       const filename = 'q' + num + '_img' + (imgIndex++) + '.png';
+      src = src.replace(/^http:\/\//i, 'https://');
       console.log('Downloading', src, 'to', filename);
       
       const file = fs.createWriteStream('images/quiz/wqt-dl10/' + filename);
-      http.get(src, function(response) {
+      https.get(src, function(response) {
+        if (response.statusCode !== 200) {
+           console.error(`Failed to download ${src}: ${response.statusCode}`);
+           return;
+        }
         response.pipe(file);
       }).on('error', function(err) {
         console.error('Error downloading:', err);
@@ -25,3 +31,4 @@ $('.res_question').each((i, qDiv) => {
     }
   });
 });
+
