2bf6a3be9420fcd6fb8a5de1629e9808644724ad
Deployer <deploy@example.com>
2026-07-31 01:35:55 +0530

    add WQT - Digital Logic-10|Multiplexer and images

--- download_images.js
+++ download_images.js
@@ -16,7 +16,7 @@ $('.res_question').each((i, qDiv) => {
       const filename = 'q' + num + '_img' + (imgIndex++) + '.png';
       console.log('Downloading', src, 'to', filename);
       
-      const file = fs.createWriteStream('images/quiz/wqt-dl9/' + filename);
+      const file = fs.createWriteStream('images/quiz/wqt-dl10/' + filename);
       http.get(src, function(response) {
         response.pipe(file);
       }).on('error', function(err) {
