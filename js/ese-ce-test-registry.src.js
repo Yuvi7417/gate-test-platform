c058387f572b84c34862fecfbb9432a73f7e301c
Deployer <deploy@example.com>
2026-08-10 21:49:26 +0530

    Add ce-ese-2026 SWT - Solid Mechanics quiz

--- js/ese-ce-test-registry.src.js
+++ js/ese-ce-test-registry.src.js
@@ -2,3 +2,12 @@
 // ESE-CE TEST REGISTRY
 // =========================================================================
 // Add quizzes/tests for ESE-CE below:
+
+
+registerTest({
+  series: "ce-ese-2026",
+  name: "SWT - Solid Mechanics",
+  date: "Oct 01, 2026",
+  questions: [
+  ]
+});
