c260546bcb4c7713c3883330696625de71d3e3d3
Deployer <deploy@example.com>
2026-08-16 15:19:02 +0530

    Cleanup temporary and sensitive files

--- check_img.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const html = fs.readFileSync('test.html', 'utf8');
-const $ = cheerio.load(html);
-console.log('Total img tags:', $('img').length);
-$('img').each((i, el) => {
-  console.log(`Image ${i}:`, $(el).attr('src'));
-});
--- scratch/fix_nat.py
+++
@@ -1,8 +0,0 @@
-import re
-with open("js/ce-test-registry.src.js", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = re.sub(r'answer:\s*"([\d\.]+)\s*-\s*\1"', r'answer: "\1"', content)
-
-with open("js/ce-test-registry.src.js", "w", encoding="utf-8") as f:
-    f.write(content)
