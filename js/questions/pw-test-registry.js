e69c349e55b59ee2b8b6f7bea1fc6e6e16d7aeb2
Yuvi7417 <yuvrajsingh36828@gmail.com>
2026-08-17 14:51:50 +0530

    Add new Algorithm quiz and download images

--- js/questions/pw-test-registry.js
+++ js/questions/pw-test-registry.js
@@ -1038,3 +1038,330 @@ Output of the program is ____`,
     },
   ]
 });
+registerTest({
+  series: "pw-cs-gate-2026",
+  name: "SWt - Algorithm",
+  date: "Oct 01, 2026",
+  questions: [
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img1.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "4",
+      solution: `<img src="/images/quiz/pw-algo/q_img2.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img3.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "D",
+      solution: `<img src="/images/quiz/pw-algo/q_img4.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img5.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "2",
+      solution: `<img src="/images/quiz/pw-algo/q_img6.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img7.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img8.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img9.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "18",
+      solution: `<img src="/images/quiz/pw-algo/q_img10.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img11.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "D",
+      solution: `<img src="/images/quiz/pw-algo/q_img12.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img13.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img14.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img15.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img16.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img17.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "11",
+      solution: `<img src="/images/quiz/pw-algo/q_img18.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img19.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "A",
+      solution: `<img src="/images/quiz/pw-algo/q_img20.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0.33,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img21.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img22.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img23.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "A",
+      solution: `<img src="/images/quiz/pw-algo/q_img24.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img25.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img26.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 1,
+      neg: 0,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img27.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img28.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img29.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "997",
+      solution: `<img src="/images/quiz/pw-algo/q_img30.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img31.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img32.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img33.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "45",
+      solution: `<img src="/images/quiz/pw-algo/q_img34.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img35.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img36.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img37.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "1500",
+      solution: `<img src="/images/quiz/pw-algo/q_img38.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img39.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "150",
+      solution: `<img src="/images/quiz/pw-algo/q_img40.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img41.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "145",
+      solution: `<img src="/images/quiz/pw-algo/q_img42.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img43.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img44.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img45.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img46.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img47.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "32",
+      solution: `<img src="/images/quiz/pw-algo/q_img48.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img49.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img50.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img51.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "A",
+      solution: `<img src="/images/quiz/pw-algo/q_img52.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img53.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "41",
+      solution: `<img src="/images/quiz/pw-algo/q_img54.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img55.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img56.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img57.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "B",
+      solution: `<img src="/images/quiz/pw-algo/q_img58.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0,
+      type: "NAT",
+      text: `<img src="/images/quiz/pw-algo/q_img59.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: [],
+      answer: "7",
+      solution: `<img src="/images/quiz/pw-algo/q_img60.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img61.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "C",
+      solution: `<img src="/images/quiz/pw-algo/q_img62.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+    {
+      marks: 2,
+      neg: 0.66,
+      type: "MCQ",
+      text: `<img src="/images/quiz/pw-algo/q_img63.png" style="display: block; max-width: 75%; margin: 10px 0;">`,
+      image: "",
+      options: ["A", "B", "C", "D"],
+      answer: "D",
+      solution: `<img src="/images/quiz/pw-algo/q_img64.png" style="display: block; max-width: 75%; margin: 10px 0;">`
+    },
+  ]
+});
