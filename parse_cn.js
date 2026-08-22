8859034aa4b3b66f119d5fd8d943edaef0a96a74
Yuvi7417 <yuvrajsingh36828@gmail.com>
2026-08-22 18:14:56 +0530

    Add SWT - DBMS-2 test to go-test-registry and rebuild

--- parse_cn.js
+++ parse_cn.js
@@ -4,10 +4,10 @@ const path = require('path');
 const https = require('https');
 
 const inputFile = 'h:\\yuvraj dutt\\test.html';
-const imgDirName = 'go-swt-dbms-1';
+const imgDirName = 'go-swt-dbms-2';
 const imgDirPath = path.join('h:\\yuvraj dutt\\images\\quiz', imgDirName);
 const seriesName = 'cs-gate-classes';
-const quizName = 'SWT - DBMS-1|Complete Database Syllabus';
+const quizName = 'SWT - DBMS-2|Complete Database Syllabus';
 const dateStr = 'Aug 22, 2026';
 
 if (!fs.existsSync(imgDirPath)) {
