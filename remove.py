8f15fe1ffce8a49312611c65d2b5763acb526d24
Yuvi7417 <yuvrajsingh36828@gmail.com>
2026-09-13 21:13:05 +0530

    Remove temporary script and log files

--- remove.py
+++
@@ -1,6 +0,0 @@
-import re
-f = 'js/questions/pw-test-registry.js'
-text = open(f, encoding='utf-8').read()
-text = re.sub(r'registerTest\(\{\s*series:\s*"pw-cs-gate-2026",\s*name:\s*"TWT-c programming",\s*date:\s*"sep 04, 2026",\s*questions:\s*\[\s*\]\s*\}\);\s*', '', text)
-open(f, 'w', encoding='utf-8').write(text)
-print("Duplicate removed.")
